6af050d96f4f5e16005ed4ebc9b7e0e48f41046f
miracleibanze <miraclecode11@gmail.com>
2024-08-26 12:56:25 +0300

    changing webpage

---
+++ script.js
@@ -0,0 +1,14 @@
+const dropIcon = document.querySelector('.hamburger');
+
+dropIcon.addEventListener('click', () => {
+    const dropMenu = dropIcon.parentElement;
+
+    if (dropMenu.classList.contains('active')) {
+        dropMenu.classList.remove('active');
+        dropIcon.innerHTML = "<img src='./assets/hamburger.svg' alt='hamburger'>"
+    }
+    else {
+        dropMenu.classList.add('active');
+        dropIcon.innerHTML = "<img src='./assets/x-tobe-sign.png' alt='hamburger'>"
+    }
+})
